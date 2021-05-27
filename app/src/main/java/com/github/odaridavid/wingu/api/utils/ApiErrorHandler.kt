6897ef9f8a7ae03b52ad7c2d4f5e534784896bfb
David Odari <davidkibzodari@gmail.com>
2021-05-27 20:58:40 +0200

    Add some loginc to the api error handler

--- app/src/main/java/com/github/odaridavid/wingu/api/utils/ApiErrorHandler.kt
+++ app/src/main/java/com/github/odaridavid/wingu/api/utils/ApiErrorHandler.kt
@@ -4,8 +4,11 @@ import com.github.odaridavid.wingu.api.models.ApiErrorResponse
 
 internal object ApiErrorHandler {
 
-    fun getUserFriendlyErrorMessage(apiErrorResponse: ApiErrorResponse): String {
-        TODO("Implementation Missing")
-    }
+    fun getUserFriendlyErrorMessage(apiErrorResponse: ApiErrorResponse): String =
+        when (apiErrorResponse.code) {
+            in 101..105 -> "An error occured with your user account"
+            in 601..615 -> "An error occured while making your request"
+            else -> apiErrorResponse.info ?: "Oops something went wrong"
+        }
 
 }
